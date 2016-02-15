a03d743e8b7d611145d4929f09ada8c8c262fe2a
tuzhao <406943270@qq.com>
2016-02-15 17:38:42 +0800

    增加全静态的SIntent

---
+++ xintent/src/main/java/com/dhy/xintent/SIntent.java
@@ -0,0 +1,71 @@
+package com.dhy.xintent;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import java.io.Serializable;
+
+/**
+ * <h3>自定义Intent对象 简化activity之间传值</h3>
+ * @author tuzhao
+ *         2016-02-15
+ */
+public class SIntent extends Intent {
+
+	/**
+	 * 根据此方法获取SIntent对象
+	 * @param packageContext Context
+	 * @param cls            Class<? extends Object>
+	 * @param serializable   Serializable
+	 * @return SIntent
+	 */
+	public static SIntent s(Context packageContext, Class<? extends Object> cls, Serializable... serializable) {
+		return new SIntent(packageContext, cls, serializable);
+	}
+
+	private SIntent(Context packageContext, Class<?> cls, Serializable... serializable) {
+		super(packageContext, cls);
+		putSerializableExtra(this, serializable);
+	}
+
+	private static void putSerializableExtra(Intent intent, Serializable... serializable) {
+		String className = intent.getComponent().getClassName();
+		intent.putExtra(className, serializable.length > 1 ? serializable : serializable[0]);
+	}
+
+	public static void putSerializableExtra(Activity activity, Serializable... serializable) {
+		putSerializableExtra(activity.getIntent(), serializable);
+	}
+
+	public static Serializable readSerializableExtra(Activity activity) {
+		return activity.getIntent().getSerializableExtra(activity.getClass().getName());
+	}
+
+	public static <T extends Serializable> T readSerializableExtra(Activity activity, Class<T> cls) {
+		return readSerializableExtra(activity, cls, null);
+	}
+
+	public static <T extends Serializable> T readSerializableExtra(Activity activity, Class<T> cls, T defaultValue) {
+		Serializable serializable = readSerializableExtra(activity);
+		if (serializable instanceof Object[]) {
+			Object[] data = (Object[]) serializable;
+			for (Object d : data) {
+				if (cls.isInstance(d)) {
+					return cls.cast(d);
+				}
+			}
+		} else if (cls.isInstance(serializable)) return cls.cast(serializable);
+		return defaultValue;
+	}
+
+	public static <T extends Serializable> T readSerializableExtra(Activity activity, Class<T> cls, int index, T defaultValue) {
+		Serializable serializable = readSerializableExtra(activity);
+		if (serializable instanceof Object[]) {
+			Object[] data = (Object[]) serializable;
+			if (index < data.length) return cls.cast(data[index]);
+		} else if (index == 0) return cls.cast(serializable);
+		return defaultValue;
+	}
+
+}
