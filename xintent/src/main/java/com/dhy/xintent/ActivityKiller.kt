142a82703ac333cc2eedf71a5957cb681f256d5e
donald <onemise@qq.com>
2018-08-27 20:01:13 +0800

    Waterfall, ActivityKiller

---
+++ xintent/src/main/java/com/dhy/xintent/ActivityKiller.kt
@@ -0,0 +1,56 @@
+package com.dhy.xintent
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import com.dhy.xintent.simple.SimpleActivityLifecycleCallbacks
+import java.util.*
+import kotlin.reflect.KClass
+
+object ActivityKiller {
+    @JvmStatic
+    fun init(application: Application) {
+        application.registerActivityLifecycleCallbacks(object : SimpleActivityLifecycleCallbacks() {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                activities.add(activity)
+            }
+
+            override fun onActivityDestroyed(activity: Activity) {
+                activities.remove(activity)
+            }
+        })
+    }
+
+    @JvmStatic
+    fun killAll() {
+        kill(activities)
+    }
+
+    @JvmStatic
+    fun killAllExcept(vararg activityClasses: KClass<out Activity>) {
+        val classes = activityClasses.map { it.java }
+        kill(activities.filter { !classes.contains(it.javaClass) })
+    }
+
+    @JvmStatic
+    fun killAllExcept(vararg activityClasses: Class<out Activity>) {
+        kill(activities.filter { !activityClasses.contains(it.javaClass) })
+    }
+
+    @JvmStatic
+    fun kill(vararg activityClasses: KClass<out Activity>) {
+        val classes = activityClasses.map { it.java }
+        kill(activities.filter { classes.contains(it.javaClass) })
+    }
+
+    @JvmStatic
+    fun kill(vararg activityClasses: Class<out Activity>) {
+        kill(activities.filter { activityClasses.contains(it.javaClass) })
+    }
+
+    private fun kill(activities: List<Activity>) {
+        activities.forEach { it.finish() }
+    }
+
+    private val activities = ArrayList<Activity>()
+}
---
+++ xintent/src/main/java/com/dhy/xintent/Waterfall.kt
@@ -0,0 +1,70 @@
+package com.dhy.xintent
+
+import android.app.Activity
+import java.util.*
+import java.util.concurrent.LinkedBlockingQueue
+
+/**
+ *@param activity for run onUiThread
+ * */
+class Waterfall(private val activity: Activity? = null) {
+    private val flowActions: Queue<(Flow) -> Unit> = LinkedBlockingQueue()
+    private var onError: ((Flow) -> Unit)? = null
+    private val results: MutableList<Any?> = mutableListOf()
+    /**
+     * on the end of {@link Flow} *SHOULD INVOKE* {@link Flow#next} when has more Flows
+     */
+    fun flow(action: (Flow) -> Unit): Waterfall {
+        flowActions.add(action)
+        return this
+    }
+
+    fun onError(onError: ((Flow) -> Unit)): Waterfall {
+        this.onError = onError
+        return this
+    }
+
+    fun start(onUiThread: Boolean = false) {
+        next(onUiThread)
+    }
+
+    private fun exit() {
+        flowActions.clear()
+        results.clear()
+    }
+
+    private fun next(onUiThread: Boolean) {
+        val action = flowActions.poll()
+        if (action != null) {
+            if (onUiThread && activity?.isFinishing != true) {
+                activity!!.runOnUiThread {
+                    action.invoke(flow)
+                }
+            } else action.invoke(flow)
+        } else exit()
+    }
+
+    private val flow = object : Flow {
+        override fun getPreResult(): Any? {
+            return results.lastOrNull()
+        }
+
+        override fun getResult(step: Int): Any? {
+            return results[step]
+        }
+
+        override fun next(result: Any?, onUiThread: Boolean) {
+            results.add(result)
+            this@Waterfall.next(onUiThread)
+        }
+
+        override fun error(result: Any?, onUiThread: Boolean) {
+            flowActions.clear()
+            if (onError != null) {
+                flowActions.add(onError)
+                onError = null
+            }
+            next(result, onUiThread)
+        }
+    }
+}
